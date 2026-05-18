7d9e6ece9a20e5004a1cd32a18a7336a98047f2f
codeMAHDI <moshiurrahmanmehedi210@gmail.com>
2026-05-19 05:40:54 +0600

    Refactor variable.js to update variable examples and clarify usage of var, let, and const

--- variable.js
+++ variable.js
@@ -10,10 +10,37 @@
 // console.log("The value of x is: " + x);
 
 
-isFollowing = true
-console.log("Am I following? " + isFollowing);
-isFollowing = false
-console.log("Am I following? " + isFollowing);
+// isFollowing = true
+// console.log("Am I following? " + isFollowing);
+// isFollowing = false
+// console.log("Am I following? " + isFollowing);
+
+
+// let, const, var
+
+// var can be re-declared and updated
+var name = "John";
+console.log(name); // Output: John
+
+name = "sjadhakus"
+console.log(name); // Output: sjadhakus
+
+
+// let can be updated but not re-declared
+let score=100
+console.log("score is: " + score); // Output: 100
+
+
+
+// const cannot be updated or re-declared
+const country = "USA";
+console.log(country); // Output: USA
+
+// country = "Canada"; // This will throw an error
+// const country = "Canada"; // This will also throw an error   
+
+
+
 
 
 
