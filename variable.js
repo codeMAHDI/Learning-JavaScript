01e5a742936643a45804c15de26d2121a30954c2
codeMAHDI <moshiurrahmanmehedi210@gmail.com>
2026-05-19 04:58:13 +0600

    variable done

---
+++ variable.js
@@ -0,0 +1,22 @@
+// age = 24
+
+
+// console.log("My age is: " + age);
+// console.log("Next year, I will be: " + (age + 1));
+
+// x=null
+// console.log("The value of x is: " + x);
+// x= undefined
+// console.log("The value of x is: " + x);
+
+
+isFollowing = true
+console.log("Am I following? " + isFollowing);
+isFollowing = false
+console.log("Am I following? " + isFollowing);
+
+
+
+
+
+
