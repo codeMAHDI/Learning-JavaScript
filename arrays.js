cb8b033c37fd1c0a7a07c6a2f0e0b76169be9871
codeMAHDI <moshiurrahmanmehedi210@gmail.com>
2026-05-22 03:57:22 +0600

    Comment out array manipulation examples in arrays.js for clarity

--- arrays.js
+++ arrays.js
@@ -1,10 +1,11 @@
 // Array 
 let arr = [1, 2, 3, 4, 5];
-console.log(arr);
-console.log(arr.length)
-arr[0]=10
-console.log(arr)
-arr.push(6) // add element to the end of the array
-console.log(arr)
-arr.pop() // remove the last element of the array
-console.log(arr)
+// console.log(arr);
+// console.log(arr.length)
+// arr[0]=10
+// console.log(arr)
+// arr.push(6) // add element to the end of the array
+// console.log(arr)
+// arr.pop() // remove the last element of the array
+// console.log(arr)
+console.log(typeof arr) // returns "object" because arrays are objects in JavaScript
