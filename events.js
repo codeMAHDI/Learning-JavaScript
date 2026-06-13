3bafebbe7b3a63b4f1112fb79d3ab4401c49f0e0
codeMAHDI <moshiurrahmanmehedi210@gmail.com>
2026-06-14 02:49:37 +0600

    app.js 50% done, completed the basic logic of tic tac toe game

---
+++ events.js
@@ -0,0 +1,7 @@
+// Events
+// What is an event?
+// An event is an action that occurs in the system 
+// you are programming, which the system tells you about so your code 
+// can respond to it. For example, when a user clicks a button on a webpage, 
+// that click is an event. The browser detects the click and can trigger a 
+// function in your code to run in response to that event.
